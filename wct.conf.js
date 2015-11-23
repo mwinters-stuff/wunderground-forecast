29d34dc2d8c558124ee1cb44c93d9aaeceed21d4
Mathew Winters <mathew_winters@trimble.com>
2015-11-24 11:38:53 +1300

    add configuration

---
+++ wct.conf.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  plugins: {
+    local: {
+      browsers: ['chrome', 'firefox']
+    },
+    sauce: {
+      browsers: ['chrome', 'firefox']
+    }
+  },
+};
