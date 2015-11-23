067ab60259a58b9b8735d914203bf6419d924216
Mathew Winters <mathew_winters@trimble.com>
2015-11-24 12:41:26 +1300

    update sauce and travis!

--- wct.conf.js
+++ wct.conf.js
@@ -1,11 +1,25 @@
 module.exports = {
-  verbose: true,
+//  verbose: true,
+  testTimeout: 5 * 60 * 1000,
   plugins: {
     local: {
       browsers: ['chrome', 'firefox']
     },
     sauce: {
-      browsers: ['chrome', 'firefox']
+      disabled: true,
+      browsers: [
+        'Linux/chrome',
+        'Linux/firefox',
+        'OS X 10.10/chrome',
+        'OS X 10.10/firefox',
+        'OS X 10.10/safari',
+        'Windows 7/internet explorer/10.0',
+        'Windows 7/internet explorer/11.0',
+        'Windows 10/MicrosoftEdge/20.10240',
+        'Windows 7/chrome',
+        'Windows 10/firefox',
+
+      ]
     }
   },
 };
